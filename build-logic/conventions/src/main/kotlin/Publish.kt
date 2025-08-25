1672e047041bfb11a7eccfe4c6dfb11356056b1c
webhead1104 <webhead1104@hotmail.com>
2025-08-25 14:47:42 -0500

    Publish stuff

--- build-logic/conventions/src/main/kotlin/Publish.kt
+++ build-logic/conventions/src/main/kotlin/Publish.kt
@@ -1,14 +1,49 @@
-import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.gradle.api.Project
+import org.gradle.authentication.http.BasicAuthentication
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
 
 internal fun Project.configureInventoryFrameworkPublication() {
-    plugins.apply("com.vanniktech.maven.publish.base")
+    plugins.apply("maven-publish")
 
-    extensions.configure<MavenPublishBaseExtension> {
-        publishToMavenCentral(automaticRelease = true)
-        signAllPublications()
-        pomFromGradleProperties()
-        configureBasedOnAppliedPlugins()
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "FoxikleCytonicRepository"
+                url = uri("https://repo.foxikle.dev/cytonic")
+                // Use providers to get the properties or fallback to environment variables
+                var u = System.getenv("REPO_USERNAME")
+                var p = System.getenv("REPO_PASSWORD")
+
+                if (u == null || u.isEmpty()) {
+                    u = "no-value-provided"
+                }
+                if (p == null || p.isEmpty()) {
+                    p = "no-value-provided"
+                }
+
+                val user = providers.gradleProperty("FoxikleCytonicRepositoryUsername").orElse(u).get()
+                val pass = providers.gradleProperty("FoxikleCytonicRepositoryPassword").orElse(p).get()
+                credentials {
+                    username = user
+                    password = pass
+                }
+                authentication {
+                    create<BasicAuthentication>("basic") {
+
+                    }
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components.getByName("java"))
+            }
+        }
     }
 }
